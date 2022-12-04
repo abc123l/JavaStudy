e2ed307740454a9cd8bba7150a2772e576f4a2c4
Diko <754956348@qq.com>
2022-12-04 21:12:15 +0800

    递归增加了汉诺塔

---
+++ src/com/abc/HanoiTower.java
@@ -0,0 +1,21 @@
+package com.abc;
+
+/**
+ * @author abc
+ * @version 1.0
+ * 汉诺塔问题
+ */
+public class HanoiTower {
+    public static void main(String[] args) {
+        hanoi('A','B','C',3);
+    }
+    public static void hanoi(char a,char b, char c,int num){
+        if (num==1){
+            System.out.println(a+"->"+c);
+        }else {
+            hanoi(a,c,b,num-1);
+            System.out.println(a+"->"+c);
+            hanoi(b,a,c,num-1);
+        }
+    }
+}
