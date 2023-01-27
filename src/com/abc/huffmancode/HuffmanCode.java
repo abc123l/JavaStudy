7ac1a3eb1e6bc91ed0f3df6f23f045e9bc7b2a6b
Diko <754956348@qq.com>
2023-01-27 20:46:51 +0800

    赫夫曼编码2(压缩和解压完成)

--- src/com/abc/huffmancode/HuffmanCode.java
+++ src/com/abc/huffmancode/HuffmanCode.java
@@ -12,23 +12,173 @@ public class HuffmanCode {
     //将赫夫曼编码表存放在Map<Byte,String>中，Byte为元素，String为赫夫曼树中从根节点到它的路径例如：<97,100>
     //存储路径时用StringBuilder
     static Map<Byte, String> huffmanCodes = new HashMap();
-    static StringBuilder stringBuilder=new StringBuilder();
+    static StringBuilder stringBuilder = new StringBuilder();
+    static int compensationZeroNumber;
+
     public static void main(String[] args) {
-        String content = "i like like like java do you like a java";
+        //String content = "i like like like java do you like a java";
+        String content="Then i go to play.";
         byte[] contentBytes = content.getBytes();
-        List<Node> nodes = getNodes(contentBytes);
-        Node huffmanTreeRoot = createHuffmanTree(nodes);
+        byte[] zipCode = huffmanZip(contentBytes);
+        System.out.println("字节数组："+Arrays.toString(zipCode));
+        System.out.println("字典："+huffmanCodes);
 
 
-        getCodes(huffmanTreeRoot);
-        System.out.println("赫夫曼编码表："+huffmanCodes);
+        byte[] originalBytes = decode(huffmanCodes, zipCode);
+        System.out.println(new String(originalBytes));
+    }
+    /*
+    数据解压：
+    1. 将zipCode转成1010100010111111110010001011....
+    2. 将1010100010111111110010001011....对照赫夫曼编码表转成原始字符串
+     */
+
+    /**
+     * 用字典和zipCode来得到contentBytes
+     * @param huffmanCodes 字典
+     * @param zipCode
+     * @return 原始字符串对应的字节数组
+     */
+    private static byte[] decode(Map<Byte,String> huffmanCodes,byte[] zipCode){
+        //得到zipCode对应的二进制字符串101010001011111.....
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < zipCode.length; i++) {
+            byte b = zipCode[i];
+            boolean flag=(i== zipCode.length-1);
+            stringBuilder.append(byteToBitString(b,!flag));//到最后一个时才会考虑补位
+        }
+        System.out.println("解码二进制数："+stringBuilder);
+        //利用字典把二进制字符串进行解码, 首先反转一下字典的key和value方便查询
+        Map<String, Byte> map = new HashMap<>();
+        Set<Map.Entry<Byte, String>> entries = huffmanCodes.entrySet();
+        for (Map.Entry<Byte, String> entry : entries) {
+            map.put(entry.getValue(),entry.getKey());
+        }
+
+        //创建一个集合存放byte，并不能直接创建一个byte数组，因为不知道大小
+        List<Byte> list = new ArrayList<>();
+        //暴力匹配
+        for (int i = 0; i < stringBuilder.length(); ) {//注意这里不能写i++否则和下面i+=gap互斥
+            int gap=1;//小计数器
+            Byte temp=null;//存放匹配到的Byte
+            while (true){
+                String key = stringBuilder.substring(i, i+gap);
+                temp = map.get(key);
+                if (temp==null){//如果在字典中没有找到该key对应的value则继续往后找
+                    gap++;
+                }else {
+                    break;
+                }
+            }
+            list.add(temp);
+            i+=gap;//让i移动到gap
+        }
+        byte[] result = new byte[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i]=list.get(i);
+        }
+
+        return result;
+    }
+    /**
+     * 将一个字符转成二进制的字符串
+     * @param b
+     * @param flag 是否需要补高位
+     * @return
+     */
+    private static String byteToBitString(byte b,boolean flag){
+
+
+
+        //由于byte没有toBinaryString方法，所以要先把byte转成int
+        int temp=b;
+
+        if (flag) {//如果是zipCode的非最后一个字节元素需要补0，例如传过来b为1那么str为1前面没有0，
+            // 而存储的zipCode之前的huffmanCode是有0的
+            temp |= 256;//256的二进制 1 0000 0000 如果temp为1 那么temp经过此运算为1 0000 0001最后切割一下就可以了
+        }
+        String str = Integer.toBinaryString(temp);//返回的二进制的补码，但有4个字节的需要压缩
+        if (flag || temp<0) {//如果最后一个元素为负数的话仍然需要切割
+            return str.substring(str.length() - 8);
+        }else {//只有是zipCode的最后一个元素且它为正数时才直接返回，不补位
+            String compensationZero="";
+            if (compensationZeroNumber>0){
+
+                for (int i = 0; i < compensationZeroNumber; i++) {
+                    compensationZero=compensationZero+"0";
+                }
+            }
+            return compensationZero+str;
+        }
+
     }
 
+    /**
+     * 赫夫曼编码，压缩
+     * @param originalBytes 原始字节数组
+     * @return 要发送的字节数组
+     */
+    private static byte[] huffmanZip(byte[] originalBytes){
+        //根据原始的字节数组创建节点
+        List<Node> nodes = getNodes(originalBytes);
+        //根据节点创建赫夫曼树
+        Node huffmanTreeRoot = createHuffmanTree(nodes);
+        //根据创建的赫夫曼树得到赫夫曼编码表
+        Map<Byte, String> huffmanCodes = getCodes(huffmanTreeRoot);
+        //根据原始字节数组和赫夫曼编码表得到要发送的字节数组
+        byte[] zipCode = zip(originalBytes, huffmanCodes);
+        return zipCode;
+    }
+    /**
+     * 将原始字符串对应的字节数组，通过赫夫曼编码表生成要发送的字节数组
+     *
+     * @param originalBytes 原始的字节数组contentBytes
+     * @param huffmanCodes
+     * @return 转换后的要发送的字节数组，每8位一组，记作huffmanCodeBytes
+     * 1010100010111111110010001011111111001000101111111100100101001101110001110000011011101000111100101000101111111100110001001010011011100
+     * 注意10101000存储的时候是补码，显示的话要转成原码
+     */
+    private static byte[] zip(byte[] originalBytes, Map<Byte, String> huffmanCodes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        //遍历originalBytes利用huffmanCodes存储要发的编码
+        for (byte b : originalBytes) {
+            stringBuilder.append(huffmanCodes.get(b));
+        }
+        //将10101000101111111100.....转成byte[]数组，首先计算要多少个字节来存储
+        System.out.println("原始二进制数："+stringBuilder);
+        int len;
+        if (stringBuilder.length() % 8 == 0) {
+            len = stringBuilder.length() / 8;
+        } else {
+            len = stringBuilder.length() / 8 + 1;
+        }
 
+        byte[] huffmanCodeBytes = new byte[len];
+        int index = 0;
+        for (int i = 0; i < stringBuilder.length(); i += 8) {//每8位一存储
+            String strByte;//临时存储8位数据
+            if (i + 8 > stringBuilder.length()) {//代表数据不够8位了
+
+                strByte = stringBuilder.substring(i);//代表从i取到末尾
+
+                while (i < stringBuilder.length()&&"0".equals(stringBuilder.charAt(i)+"")  ){//如果最后不足8位的正数前面有0的话
+                    compensationZeroNumber++;
+                    i++;
+                }
+
+            } else {
+                strByte = stringBuilder.substring(i, i + 8);
+            }
+            huffmanCodeBytes[index] = (byte) Integer.parseInt(strByte, 2);
+            index++;
+        }
+        return huffmanCodeBytes;
+    }
 
     /**
      * 用于获取霍夫曼编码，放在huffmanCodes里
      * 注意为什么要StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
+     *
      * @param node          霍夫曼树的根节点
      * @param path          路径：左子节点是0，右子节点是1
      * @param stringBuilder 用于拼接路径
@@ -50,15 +200,16 @@ public class HuffmanCode {
 
     /**
      * 方法重载
+     *
      * @param huffmanTreeRoot
      * @return
      */
-    private static Map<Byte, String> getCodes(Node huffmanTreeRoot){
-        if (huffmanTreeRoot==null){
+    private static Map<Byte, String> getCodes(Node huffmanTreeRoot) {
+        if (huffmanTreeRoot == null) {
             return null;
-        }else {
-            getCodes(huffmanTreeRoot.left,"0",stringBuilder);
-            getCodes(huffmanTreeRoot.right,"1",stringBuilder);
+        } else {
+            getCodes(huffmanTreeRoot.left, "0", stringBuilder);
+            getCodes(huffmanTreeRoot.right, "1", stringBuilder);
             return huffmanCodes;
         }
     }
